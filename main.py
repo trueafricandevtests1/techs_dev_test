cdc7da88b575a2262dd5ebacf8d734b82cf76f53
rkimera <rkimera2013@gmail.com>
2022-04-30 13:44:18 +0300

    occurence of recipes

--- main.py
+++ main.py
@@ -25,8 +25,40 @@ class stats_class(object):
         filtered = {'unique_recipe_count': len(arr_uniq_recipes)}
         return filtered
 
+    def postal(self):
+        data = self.data
+
+    def occurrence_of_recipes(self):
+        data = self.data
+        finalList = []
+
+        for i in range(len(data)):
+            if i == 0:
+                recipe_count = {'delivery': data[i]['delivery'],
+                                'recipe': data[i]['recipe'], 'postcode': data[i]['postcode'], 'count': 1}
+                finalList.append(recipe_count)
+
+            elif i != 0:
+                '''
+                and [lst['count'] + 1 for lst in finalList if lst['recipe']
+                           == data[i]['recipe']]:
+                print(i)
+                '''
+
+                for v in range(len(finalList)):
+                    if finalList[v]['recipe'] != data[i]['recipe']:
+                        recipe_count = {'delivery': data[i]['delivery'],
+                                        'recipe': data[i]['recipe'], 'postcode': data[i]['postcode'], 'count': 1}
+                        finalList.append(recipe_count)
+                for v in range(len(finalList)):
+                    if finalList[v]['recipe'] == data[i]['recipe']:
+                        finalList[v]['count'] = finalList[v]['count']+1
+                i = +1
+        print(finalList)
+
 
 # call the class
 stats = stats_class(data)
 print(stats.uniq_recipe_element()
       )
+print(stats.occurrence_of_recipes())
